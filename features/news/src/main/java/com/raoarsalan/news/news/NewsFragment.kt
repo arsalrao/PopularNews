91f148b45e9bdb0013aaf39c1be36bf56a437f3f
Rao Arsalan <raoarsalan30@hotmail.com>
2022-07-02 20:58:17 +0400

    load news data

--- features/news/src/main/java/com/raoarsalan/news/news/NewsFragment.kt
+++ features/news/src/main/java/com/raoarsalan/news/news/NewsFragment.kt
@@ -1,14 +1,40 @@
 package com.raoarsalan.news.news
 
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.raoarsalan.base.BaseFragment
+import com.raoarsalan.core.domain.model.response.NewsModel
 import com.raoarsalan.news.BR
+import com.raoarsalan.news.adapter.NewsAdapter
 import com.raoarsalan.news.databinding.FragmentNewsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NewsFragment : BaseFragment<FragmentNewsBinding, NewsVM>(NewsVM::class.java) {
+class NewsFragment : BaseFragment<FragmentNewsBinding, NewsVM>(NewsVM::class.java),
+    NewsAdapter.OnItemClickListener {
     override val viewModelBindingVariable: Int
         get() = BR.viewModel
 
     override fun setBinding() = FragmentNewsBinding.inflate(layoutInflater)
+
+    private val adapter = NewsAdapter(this)
+
+    override fun initViews() {
+        super.initViews()
+        viewModel.getPopularNews()
+
+        binding.rvNews.let {
+            it.adapter = adapter
+            it.layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    override fun onItemClick(item: NewsModel) {}
+
+    override fun observers() {
+        super.observers()
+        viewModel.observeNews.observe(viewLifecycleOwner, Observer {
+            adapter.submitList(it)
+        })
+    }
 }
--- features/news/src/main/java/com/raoarsalan/news/news/NewsVM.kt
+++ features/news/src/main/java/com/raoarsalan/news/news/NewsVM.kt
@@ -1,7 +1,35 @@
 package com.raoarsalan.news.news
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.raoarsalan.base.BaseViewModel
+import com.raoarsalan.base.utill.SingleLiveEvent
+import com.raoarsalan.core.domain.Result
+import com.raoarsalan.core.domain.interactor.NewsInteractor
+import com.raoarsalan.core.domain.model.response.NewsModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class NewsVM @Inject constructor() : BaseViewModel() {
+class NewsVM @Inject constructor(
+    private val newsInteractor: NewsInteractor
+) : BaseViewModel() {
+
+    private val _observeNews = SingleLiveEvent<List<NewsModel>>()
+    val observeNews: LiveData<List<NewsModel>> = _observeNews
+
+    fun getPopularNews() {
+        viewModelScope.launch {
+            when (val res = newsInteractor.getPopularNews()) {
+                is Result.Success -> {
+                    _observeNews.postValue(res.data.results)
+                }
+
+                is Result.GenericError -> {
+                }
+
+                is Result.NetworkError -> {
+                }
+            }
+        }
+    }
 }
