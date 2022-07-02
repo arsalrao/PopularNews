8234fa967108b06460821c5a39a00b68f6b190c4
Rao Arsalan <raoarsalan30@hotmail.com>
2022-07-02 18:34:47 +0400

    implement repository

--- framework/src/main/java/com/raoarsalan/framework/repositoryimpl/NewsRepositoryImpl.kt
+++ framework/src/main/java/com/raoarsalan/framework/repositoryimpl/NewsRepositoryImpl.kt
@@ -4,13 +4,20 @@ import com.raoarsalan.core.domain.Result
 import com.raoarsalan.core.domain.ResultResponse
 import com.raoarsalan.core.domain.data.repository.NewsRepository
 import com.raoarsalan.core.domain.model.response.NewsModel
+import com.raoarsalan.framework.network.ApiCall
 import com.raoarsalan.framework.network.apis.NewsApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class NewsRepositoryImpl @Inject constructor(
     private val api: NewsApi
 ) : NewsRepository {
     override suspend fun getPopularNews(): Result<ResultResponse<NewsModel>> {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            return@withContext ApiCall.result {
+                api.getPopularNews()
+            }
+        }
     }
 }
