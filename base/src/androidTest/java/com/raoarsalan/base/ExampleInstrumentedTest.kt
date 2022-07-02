3f62ee57103cf8d609e7c07e522fad2b59977106
Rao Arsalan <raoarsalan30@hotmail.com>
2022-07-02 12:19:32 +0400

    Refactor

--- base/src/androidTest/java/com/raoarsalan/base/ExampleInstrumentedTest.kt
+++ base/src/androidTest/java/com/raoarsalan/base/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.raoarsalan.core
+package com.raoarsalan.base
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
--- base/src/test/java/com/raoarsalan/base/ExampleUnitTest.kt
+++ base/src/test/java/com/raoarsalan/base/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.raoarsalan.core
+package com.raoarsalan.base
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
