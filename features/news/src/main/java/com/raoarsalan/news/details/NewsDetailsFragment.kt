5561d3076492c3ffd7441b4c92def8d4ad6ec97d
raoarsalan <raoarsalan30@gmail.com>
2022-07-03 18:16:31 +0400

    Implement navigation toolbar

--- features/news/src/main/java/com/raoarsalan/news/details/NewsDetailsFragment.kt
+++ features/news/src/main/java/com/raoarsalan/news/details/NewsDetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.raoarsalan.news.details
 
 import com.raoarsalan.base.BaseFragment
+import com.raoarsalan.base.utill.NavigationCommand
 import com.raoarsalan.news.BR
 import com.raoarsalan.news.databinding.FragmentNewsDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,4 +13,11 @@ class NewsDetailsFragment :
         get() = BR.viewModel
 
     override fun setBinding() = FragmentNewsDetailsBinding.inflate(layoutInflater)
+
+    override fun listeners() {
+        super.listeners()
+        binding.toolbar.setNavigationOnClickListener {
+            viewModel.navigationCommands.value = NavigationCommand.Back
+        }
+    }
 }
